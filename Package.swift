56bbd5452cc69427a2c34d321205d64638486bd7
Mudit Goyal <mudit2610@gmail.com>
2026-01-09 18:51:15 +0530

    added SPM support

--- Package.swift
+++ Package.swift
@@ -1,3 +1,9 @@
 // swift-tools-version:5.9
 import PackageDescription
-let package = Package( name: "CardIO", platforms: [.iOS(.v6_1)], products: [ .library(name: "CardIO", targets: ["CardIO"]) ], targets: [ .target( name: "CardIO", path: "Sources/CardIO", publicHeadersPath: ".", linkerSettings: [ .linkedFramework("Accelerate"), .linkedFramework("AVFoundation"), .linkedFramework("AudioToolbox"), .linkedFramework("CoreMedia"), .linkedFramework("MobileCoreServices"), .linkedLibrary("c++") ] ) ] )
+let package = Package(
+    name: "CardIO",
+    platforms: [.iOS(.v15)],
+    products: [ .library(name: "CardIO", targets: ["CardIO"]) ],
+    targets: [
+        .target( name: "CardIO", path: "Sources/CardIO", publicHeadersPath: ".",
+                 linkerSettings: [ .linkedFramework("Accelerate"), .linkedFramework("AVFoundation"), .linkedFramework("AudioToolbox"), .linkedFramework("CoreMedia"), .linkedFramework("MobileCoreServices"), .linkedLibrary("c++") ] ) ] )
