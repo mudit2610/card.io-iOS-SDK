613df64829b5376ec1628f43210c9092022c90aa
Mudit Goyal <mudit2610@gmail.com>
2026-01-14 17:53:22 +0530

    swift build

--- .build/arm64-apple-macosx/debug/CardIO.build/CardIO.m.d
+++ .build/arm64-apple-macosx/debug/CardIO.build/CardIO.m.d
@@ -1,3 +1,3 @@
 dependencies: \
   /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX15.5.sdk/usr/include/Darwin.modulemap \
-  /Users/mudit.goyal/card.io-iOS-SDK/Sources/CardIO/CardIO.m
+  /Users/mudit.goyal/card.io-iOS-SDK/CardIO/CardIO.m
